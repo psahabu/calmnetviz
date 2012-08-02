7e4dc2e4f439971778c621468a3b4c91a630cf70
sahabp <sahabp@uw.edu>
2012-08-01 22:33:28 -0700

    Nothing important changed.

--- src/com/p2pbr/netviz/Runner.java
+++ src/com/p2pbr/netviz/Runner.java
@@ -10,9 +10,16 @@ public class Runner {
 			return;
 		}
 	    System.loadLibrary("jpcap");
+	    
+/*	 Compiler was unhappy with this code:
+ * java.lang.RuntimeException: java.lang.ClassNotFoundException: com.p2pbr.netviz.Net${target}
+
 	    if (!args[0].contains("Net")) {
 			PApplet.main(new String[] { "--present", "com.p2pbr.netviz.Net" + args[0] });
-	    } else if (args[0].compareToIgnoreCase("TorNetViz") == 0) {
+	    } else 
+*/
+
+		if (args[0].compareToIgnoreCase("TorNetViz") == 0) {
 			if (args.length != 5) {
 				System.out.println("TorNetViz requires four arguments:");
 				System.out.println("Directory containing data files | starting time | ending time | seconds per one day of data");
@@ -20,8 +27,15 @@ public class Runner {
 				return;
 			}
 			PApplet.main(new String[] { "--present", "com.p2pbr.netviz.TorNetViz", args[1], args[2], args[3], args[4] });
-		} else {
+		}
+		
+/*	Compiler was also unhappy with this code:
+ * java.lang.RuntimeException: java.lang.ClassNotFoundException: com.p2pbr.netviz.${target}
+		
+		else {
 			PApplet.main(new String[] { "--present", "com.p2pbr.netviz." + args[0] });
-	    }
+	    }  
+*/
+ 
 	}
 }
